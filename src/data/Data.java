a123487fdeef587f373a93bf46626b59096ab91d
cadcri <cadcri@murloc.home>
2023-12-06 23:16:27 +0000

    ok

--- src/data/Data.java
+++ src/data/Data.java
@@ -13,9 +13,9 @@ public class Data {
         data.put("client1", new NodeData(new String[0],"10.0.0.20", new String[]{"10.0.0.21"}));
         data.put("client2", new NodeData(new String[0],"10.0.4.21", new String[]{"10.0.4.20"}));
         data.put("client3", new NodeData(new String[0],"10.0.1.21", new String[0]));
-        data.put("client4", new NodeData(new String[0],"10.0.2.21", new String[0]));
-        data.put("node1", new NodeData(new String[0],"10.0.6.21", new String[]{"10.0.2.1"}));
+        data.put("client4", new NodeData(new String[0],"10.0.2.21", new String[]{"10.0.2.20"}));
+        data.put("node1", new NodeData(new String[0],"10.0.6.21", new String[]{"10.0.6.20"}));
         data.put("node2", new NodeData(new String[0],"10.0.3.20", new String[]{"10.0.3.21"}));
-        data.put("node3", new NodeData(new String[0],"10.0.7.21", new String[0]));
+        data.put("node3", new NodeData(new String[0],"10.0.7.21", new String[]{"10.0.7.20"}));
     }
 }
